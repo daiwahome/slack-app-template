ea290a745fc4f806fd02d72c9681befd8565c690
Kohei Yamato <daiwahome0@gmail.com>
2019-10-05 12:55:55 +0900

    Setup bolt framework

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,23 @@
 import * as functions from 'firebase-functions';
+import { App, ExpressReceiver } from '@slack/bolt';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const config = functions.config();
+const expressReceiver = new ExpressReceiver({
+  signingSecret: config.slack.signing_secret,
+  endpoints: '/',
+});
+const app = new App({
+  receiver: expressReceiver,
+  token: config.slack.bot_token,
+});
+app.error(console.log);
+
+app.command('/hello', async ({ ack, say }) => {
+  ack();
+  say("Hello");
+});
+
+exports.slack = functions
+  .region('asia-northeast1')
+  .https
+  .onRequest(expressReceiver.app);
